9b514bd6cb2ddc9b6c09520e7feb77b4e49142d7
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-04-16 17:17:52 -0700

    Form handling - template + controller + route definition + for as mixin

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,11 @@
 exports.homepage = (req,res) => {
     res.render('index')
 }
+
+exports.addStore = (req,res) => {
+  res.render('editStore', {title: 'Add Store'})
+}
+
+exports.createStore = (req,res) => {
+  res.json(req.body);
+}
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,7 @@ const storeController = require('../controllers/storeController');
 
 // Do work here
 router.get('/', storeController.homepage)
+router.get('/add', storeController.addStore)
+router.post('/add', storeController.createStore)
 
 module.exports = router;
