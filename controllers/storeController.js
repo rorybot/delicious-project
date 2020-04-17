5f92c688c85bb0a99c1ff7fae7291d1941aaf439
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-04-17 15:30:45 -0700

    Store model takes location info

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -11,7 +11,6 @@ exports.addStore = (req,res) => {
 
 exports.createStore = async (req,res) => {
   const store = await(new Store(req.body)).save();
-  console.log(req.body)
   req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
 }
