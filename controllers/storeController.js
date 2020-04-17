4e9a1ae52d9ec18e1670fa3dcdb6ffad32073166
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-04-17 10:57:45 -0700

    Displays list of stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -11,6 +11,13 @@ exports.addStore = (req,res) => {
 
 exports.createStore = async (req,res) => {
   const store = await(new Store(req.body)).save();
+  console.log(req.body)
   req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
+}
+
+exports.getStores = async (req,res) => {
+  const stores = await Store.find();
+  console.log(stores)
+  res.render('stores', {title: 'Stores', stores})
 }
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ const storeController = require('../controllers/storeController');
 const { catchErrors } = require('../handlers/errorHandlers')
 
 // Do work here
-router.get('/', storeController.homepage)
-router.get('/add', storeController.addStore)
-router.post('/add', storeController.createStore)
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores',catchErrors(storeController.getStores));
+router.get('/add', storeController.addStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
