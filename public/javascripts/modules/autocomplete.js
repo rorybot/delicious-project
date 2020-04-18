8c948641fa1b343284ea623b12fe0951af4ee7b2
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-04-17 17:41:03 -0700

    Fix wrong lat/lng grab

--- public/javascripts/modules/autocomplete.js
+++ public/javascripts/modules/autocomplete.js
@@ -4,7 +4,7 @@ function autocomplete(input, latInput, lngInput){
   dropdown.addListener('place_changed', () => {
     const place = dropdown.getPlace();
     latInput.value = place.geometry.location.lat();
-    lngInput.value = place.geometry.location.lat();
+    lngInput.value = place.geometry.location.lng();
   })
   input.on('keydown', (e) => {
     if(e.keyCode ===13) e.preventDefault();
