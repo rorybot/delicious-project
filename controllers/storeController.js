836bf54930fc02f954e84daf70e5443602a40c26
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-04-18 10:20:28 -0700

    Accepts image uploads and resizes them

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 const multer = require('multer');
 const multerOptions = {
-  storage: multer.memorytorage(),
+  storage: multer.memoryStorage(),
   fileFilter(req,file,next){
     const isPhoto = file.mimetype.startsWith('image/');
     if(isPhoto){
@@ -12,6 +12,8 @@ const multerOptions = {
     }
   }
 }
+const jimp = require('jimp');
+const uuid = require('uuid');
 
 exports.homepage = (req,res) => {
     res.render('index')
@@ -22,6 +24,19 @@ exports.addStore = (req,res) => {
 }
 
 exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  if(!req.file){
+    next();
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  next();
+}
 
 exports.createStore = async (req,res) => {
   const store = await(new Store(req.body)).save();
