8843a8d2c42e1d7cee518c334f7677906a975c3d
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-04-20 13:30:00 -0700

    Form validate function

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,4 +2,29 @@ const mongoose = require('mongoose');
 
 exports.loginForm = (req,res) => {
   res.render('login', {title: 'Login'})
-}
+};
+exports.registerForm = (req, res) => {
+  res.render('register', {title: 'Register'})
+};
+
+exports.validateRegister = (req, res, next) => {
+  req.sanitizeBody('name');
+  req.checkBody('name', 'You must supply a name!').notEmpty();
+  req.checkBody('email', 'That\'s not a valid email!').isEmail();
+  req.sanitizeBody('email').normalizeEmail({
+    remove_dots: false,
+    remove_extension: false,
+    gmail_remove_subaddress: false
+  })
+  req.checkBody('password', 'Password cannot be blank!').notEmpty();
+  req.checkBody('password-confirm', 'Must provide password confirmation!')
+  req.checkBody('password-confirm', 'Oops! Your passwords do not match!').equals(req.body.password)
+
+  const errors = req.validationErrors();
+  if(errors){
+    req.flash('error', errors.map(err=>err.msg))
+    res.render('register', {title: 'Register', body: req.body, flashes: req.flash()});
+    return;
+  }
+  next();
+};
