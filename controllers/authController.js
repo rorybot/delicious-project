9e80536db7dadf5d062879d72926a9af7d5aaa59
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-04-20 14:14:14 -0700

    Added route protection and applied to GET->/add route

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,3 +12,12 @@ exports.logout = (req,res) => {
   req.flash('success', 'You are now logged out');
   res.redirect('/');
 }
+
+exports.isLoggedIn = (req,res,next) => {
+  if(req.isAuthenticated()){
+    next();
+    return;
+  }
+  req.flash('error', 'Oops! You must be logged in to do that.');
+  res.redirect('/login');
+}
