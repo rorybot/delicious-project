4a8932d1fd6d50b46a73b5cd7261013ebfe9be18
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-04-20 14:06:13 -0700

    handles logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,3 +6,9 @@ exports.login = passport.authenticate('local', {
   successRedirect: '/',
   successFlash: 'Successful login!'
 })
+
+exports.logout = (req,res) => {
+  req.logout();
+  req.flash('success', 'You are now logged out');
+  res.redirect('/');
+}
