58badbf863dc60e3b321ff9b2a0ddee57f9f152e
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-04-21 15:02:31 -0700

    Dropdown results added to search bar; sanitises output too

--- public/javascripts/modules/typeAhead.js
+++ public/javascripts/modules/typeAhead.js
@@ -1,4 +1,5 @@
-const axios = require('axios');
+import axios from 'axios';
+import dompurify from 'dompurify';
 
 function searchResultsHTML(stores){
   return stores.map( store => {
@@ -29,8 +30,10 @@ function typeAhead(search){
       .then(res => {
         if(res.data.length){
           const html = searchResultsHTML(res.data);
-          searchResults.innerHTML = html;
+          searchResults.innerHTML = dompurify.sanitize(html);
+          return;
         }
+        searchResults.innerHTML = dompurify.sanitize(`<div class="search__result">No results for ${this.value} found!</div>`);
       }).catch(
         err => {
           console.error(err)
@@ -38,7 +41,30 @@ function typeAhead(search){
       );
   });
 
+  searchInput.on('keyup', (e)=> {
+    if(![38,40,13].includes(e.keyCode)){
+      return;
+    }
+    const activeClass = 'search__result--active';
+    const current = search.querySelector(`.${activeClass}`);
+    const items = search.querySelectorAll('.search__result');
+    let next;
+    if(e.keyCode === 40 && current){
+      next = current.nextElementSibling || items[0];
+    } else if(e.keyCode === 40){
+      next = items[0];
+    } else if(e.keyCode === 38 && current){
+      next = current.previousElementSibling || items[items.length - 1];
+    } else if(e.keyCode === 38){
+      next = items[items.length -1];
+    }else if (e.keyCode === 13 && current.href){
+      window.location = current.href;
+      return;
+    }
 
+    if(current) current.classList.toggle(activeClass);
+    next.classList.toggle(activeClass);
+  })
 };
 
 export default typeAhead;
