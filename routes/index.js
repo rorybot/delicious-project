69a492c01273c250aebad9a7f62bc8cdabdbc243
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-04-21 14:05:30 -0700

    API endpoint implemented for search stores text + desc

--- routes/index.js
+++ routes/index.js
@@ -45,4 +45,10 @@ router.post('/account/reset/:token',
   authController.confirmedPasswords, catchErrors(authController.updatePassword)
 );
 
+/*
+  API
+*/
+
+router.get('/api/search', catchErrors(storeController.searchStores));
+
 module.exports = router;
