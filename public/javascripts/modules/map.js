73be96bd04ee965c079df5a5e38a212b9ef8a4e8
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-04-21 16:12:15 -0700

    Load and execute map on map page pug file

---
+++ public/javascripts/modules/map.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const mapOptions = {
+  center: { lat: 43.2, lng: -79.8 },
+  zoom: 2
+}
+
+function loadPlaces(map, lat = 43.2, lng = -79.8) {
+
+}
+
+function makeMap(mapDiv) {
+    if(!mapDiv) return;
+    const map = new google.maps.Map(mapDiv, mapOptions)
+}
+
+export default makeMap;
