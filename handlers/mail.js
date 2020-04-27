c871dd2960273f38a509b529ee144f018fded498
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-04-26 17:15:54 -0700

    Swapout mailgun if not in development

--- handlers/mail.js
+++ handlers/mail.js
@@ -3,15 +3,32 @@ const pug = require('pug');
 const juice = require('juice');
 const htmlToText = require('html-to-text');
 const promisify = require('es6-promisify');
+const mg = require('nodemailer-mailgun-transport');
 
-const transport = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
+
+
+const mailgunOptions = {
   auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS
+    api_key: process.env.MAILGUN_ACTIVE_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN
   }
-});
+}
+
+const getTransport = function(){
+  if (process.env.NODE_ENV === 'development') {
+    return nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
+      }
+    });
+  } else {
+    return nodemailer.createTransport(mg(mailgunOptions));
+  }
+}
+
 
 const generateHTML = (filename, options = {}) => {
   const html = pug.renderFile(`${__dirname}/../views/email/${filename}.pug`, options);
@@ -19,11 +36,13 @@ const generateHTML = (filename, options = {}) => {
 }
 
 exports.send = async (options) => {
+  const transport = getTransport();
   const html = generateHTML(options.filename,options)
   const text = htmlToText.fromString(html);
   const maiLOptions = {
     from: 'Rory <noreply@rorybot.com>',
     to: options.user.email,
+    subject: "Password Reset",
     html,
     text
   };
